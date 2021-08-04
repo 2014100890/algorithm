947c23f0aa65fc02cb5be21c347375100d39f603
2014100890 <2014100890@khu.ac.kr>
2021-08-04 16:24:26 +0900

    0804 solve(heapq)

---
+++ 더맵게.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    
+    while scoville[0] < K : 
+        if(len(scoville) >=2 ) : 
+            heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2 )
+            answer += 1
+        else : 
+            answer = -1
+            break
+            
+    return answer
